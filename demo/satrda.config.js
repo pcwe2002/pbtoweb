2a4c7da54df407b74bbbba2d487534257f5f061d
only <pcwe2002@163.com>
2023-09-17 00:30:10 +0800

    add demo page

---
+++ demo/satrda.config.js
@@ -0,0 +1,9 @@
+(function(){
+    window.baseConfig = {
+        "title":"SatWeb框架",
+        "webapp":"test",
+        "api":"/erp",
+        "debug":false,
+        "index":"/data/page/index.js",
+    }
+})();
